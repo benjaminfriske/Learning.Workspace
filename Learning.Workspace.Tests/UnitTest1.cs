a66f329dfb1b7678d867c9a664fd8b7c43f41812
benjaminfriske <benjaminfriske@gmail.com>
2020-12-26 16:05:11 -0600

    Init commit for ms tests to workspace solution.

---
+++ Learning.Workspace.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Learning.Workspace.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
